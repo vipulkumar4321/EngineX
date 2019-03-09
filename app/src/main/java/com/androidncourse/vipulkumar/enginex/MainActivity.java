47e38a662d2cc3dd5e51014e49e4d93c08c0d65c
vipulkumar4321 <vipulkumar4321@gmail.com>
2019-03-09 12:56:12 +0530

    test enginex

---
+++ app/src/main/java/com/androidncourse/vipulkumar/enginex/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidncourse.vipulkumar.enginex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
